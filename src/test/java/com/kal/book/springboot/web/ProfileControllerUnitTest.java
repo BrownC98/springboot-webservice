7db130bedaee242236e76cdc190590042ead76b5
dongkeon Kal <0429donggun@gmail.com>
2020-01-13 22:41:09 +0900

    무중단 배포 컨트롤러 작성

---
+++ src/test/java/com/kal/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,59 @@
+package com.kal.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void read_real_profile(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void no_real_profile_then_read_first_profile() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void no_active_profile_then_default() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
